bb9cd1c1bead60f89de5e0a84b16be01d2ec2964
utsavadhikari1 <irakihda.vastu@gmail.com>
2023-10-02 22:16:11 +0545

    resize in box

--- src/components/RashifalBox.js
+++ src/components/RashifalBox.js
@@ -2,9 +2,112 @@ import React from "react";
 
 const RashifalBox = () => {
   return (
-   
     <div className="grid grid-cols-3 gap-4 rounded-lg">
-      <div className="px-2 py-4 w-64 h-32 rounded-lg mx-3 border border-orange-600" >
+{/* 
+      <div className=" rounded-lg p-5">
+        <div className="bg-orange-600 text-white py-2 px-4 rounded-t-lg">
+          <h2 className="text-1xl font-semibold"> मेष ( चु, चे, चो, ला, लि, लु, ले, लो, अ )</h2>
+        </div>
+        <div className="p-4">
+          <p>This is the content of the box.</p>
+        </div>
+      </div>
+      <div className="rounded-lg p-5">
+        <div className="bg-orange-600 text-white py-2 px-4 rounded-t-lg">
+          <h2 className="text-1xl font-semibold">वृष ( इ, उ, ए, ओ, वा, वि, वु, वे, वो )</h2>
+        </div>
+        <div className="p-4">
+          <p>This is the content of the box.</p>
+        </div>
+      </div>
+      <div className="rounded-lg p-5">
+        <div className="bg-orange-600 text-white py-2 px-4 rounded-t-lg">
+          <h2 className="text-1xl font-semibold">मिथुन ( का, कि, कु, घ, ङ, छ, के, को, हा )</h2>
+        </div>
+        <div className="p-4">
+          <p>This is the content of the box.</p>
+        </div>
+      </div>
+      <div className="rounded-lg p-5">
+        <div className="bg-orange-600 text-white py-2 px-4 rounded-t-lg">
+          <h2 className="text-1xl font-semibold">कर्कट ( हि, हु, हे, हो, डा, डि, डु, डे, डो )</h2>
+        </div>
+        <div className="p-4">
+          <p>This is the content of the box.</p>
+        </div>
+      </div>
+      <div className="rounded-lg p-5">
+        <div className="bg-orange-600 text-white py-2 px-4 rounded-t-lg">
+          <h2 className="text-1xl font-semibold">सिंह ( मा, मि, मु, मे, मो, टा, टि, टु, टे )</h2>
+        </div>
+        <div className="p-4">
+          <p>This is the content of the box.</p>
+        </div>
+      </div>
+      <div className="rounded-lg p-5">
+        <div className="bg-orange-600 text-white py-2 px-4 rounded-t-lg">
+          <h2 className="text-1xl font-semibold"> कन्या ( टो, पा, पि, पु, ष, ण, ठ, पे, पो )</h2>
+        </div>
+        <div className="p-4">
+          <p>This is the content of the box.</p>
+        </div>
+      </div>
+
+      <div className="rounded-lg p-5">
+        <div className="bg-orange-600 text-white py-2 px-4 rounded-t-lg">
+          <h2 className="text-1xl font-semibold">तुला ( रा, रि, रु, रे, रो, ता, ति, तु, ते )</h2>
+        </div>
+        <div className="p-4">
+          <p>This is the content of the box.</p>
+        </div>
+      </div>
+
+      <div className="rounded-lg p-5">
+        <div className="bg-orange-600 text-white py-2 px-4 rounded-t-lg">
+          <h2 className="text-1xl font-semibold">वृश्चिक ( तो, ना, नि, नु, ने, नो, या, यि, यु )</h2>
+        </div>
+        <div className="p-4">
+          <p>This is the content of the box.</p>
+        </div>
+      </div>
+
+      <div className="rounded-lg p-5">
+        <div className="bg-orange-600 text-white py-2 px-4 rounded-t-lg">
+          <h2 className="text-1xl font-semibold">धनु ( ये, यो, भा, भि, भु, धा, फा, ढा, भे )</h2>
+        </div>
+        <div className="p-4">
+          <p>This is the content of the box.</p>
+        </div>
+      </div>
+
+      <div className="rounded-lg p-5">
+        <div className="bg-orange-600 text-white py-2 px-4 rounded-t-lg">
+          <h2 className="text-1xl font-semibold">मकर (भो,जा,जि,जु,जे,जो,ख,खि,खु,खे,खो,गा,गि)</h2>
+        </div>
+        <div className="p-4">
+          <p>This is the content of the box.</p>
+        </div>
+      </div>
+
+      <div className="rounded-lg p-5">
+        <div className="bg-orange-600 text-white py-2 px-4 rounded-t-lg">
+          <h2 className="text-1xl font-semibold">कुम्भ ( गु, गे, गो, सा, सि, सु, से, सो, दा )</h2>
+        </div>
+        <div className="p-4">
+          <p>This is the content of the box.</p>
+        </div>
+      </div>
+      
+      <div className="rounded-lg p-5">
+        <div className="bg-orange-600 text-white py-2 px-4 rounded-t-lg">
+          <h2 className="text-1xl font-semibold">मीन ( दि, दु, थ, झ, ञ, दे, दो, चा, चि )</h2>
+        </div>
+        <div className="p-4">
+          <p>This is the content of the box.</p>
+        </div>
+      </div> */}
+
+      <div className="bg-white p-5 " >
         <button className="bg-orange-600 my-0 mr-1 text-white font-bold py-2 px-4 rounded-lg">
           मेष ( चु, चे, चो, ला, लि, लु, ले, लो, अ )
         </button>
@@ -65,7 +168,6 @@ const RashifalBox = () => {
         </button>
       </div>
     </div>
-   
   );
 };
 
